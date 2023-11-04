3a1d52c4ca9e1de7c7fd59eeb40fa80b48aef4bf
shimizu-saffle <shimizu.saffle@gmail.com>
2023-11-04 13:56:30 +0900

    feat: add command-line argument and description

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
 use reqwest::blocking::Client;
-use std::env;
+use clap::Parser;
+
+/// This program sends an HTTP request based on the provided method (GET or POST) to the specified URL.
+/// It then prints the URL accessed, the response headers, the status code, and the response body.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The URL to which the HTTP request will be sent. This argument is required and should be provided on the command line.
+    #[arg(short, long)]
+    url: String,
+
+    /// The HTTP method to be used for the request. This argument is required and should be provided on the command line.
+    /// Valid methods include "GET", "POST", etc.
+    #[arg(short, long)]
+    method: String,
+}
 
 fn main() {    
-    let args: Vec<String> = env::args().collect();
-    println!("args: {:?}", args);
-
-    let url = args[1].clone();
+    let args = Args::parse();
     let client = Client::new();
-    let response = match client.get(url)
-    .send() {
-        Ok(response) => response,
-        Err(e) => panic!("request failed:{}", e)
+    let response = match args.method.as_str() {
+        "GET" => {
+            match client.get(&args.url).send() {
+                Ok(response) => response,
+                Err(e) => panic!("GET request failed: {}", e),
+            }
+        },
+        "POST" => {
+            match client.post(&args.url).send() {
+                Ok(response) => response,
+                Err(e) => panic!("POST request failed: {}", e),
+            }
+        },        
+        _ => panic!("Unsupported HTTP method"),
     };
 
     println!("Access {}", response.url());
     println!("Header {:?}", response.headers());
+    println!("Status {}", response.status());
     println!("body = {:?}", response.text());
 }
