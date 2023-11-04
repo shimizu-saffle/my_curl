2d0c946480aaed4af296d96e68c55292d1d0513a
shimizu-saffle <shimizu.saffle@gmail.com>
2023-11-04 12:50:56 +0900

    feat: url を引数で受け取れるようにする

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use reqwest::blocking::Client;
+use std::env;
 
 fn main() {    
+    let args: Vec<String> = env::args().collect();
+    println!("args: {:?}", args);
+
+    let url = args[1].clone();
     let client = Client::new();
-    let response = match client.get("http://localhost:3000")
+    let response = match client.get(url)
     .send() {
         Ok(response) => response,
-        Err(e) => panic!("Request failed:{}", e)
-    };         
+        Err(e) => panic!("request failed:{}", e)
+    };
 
     println!("Access {}", response.url());
     println!("Header {:?}", response.headers());
